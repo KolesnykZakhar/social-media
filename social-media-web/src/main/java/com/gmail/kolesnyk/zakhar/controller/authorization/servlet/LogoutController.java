02a6ac2def51e6816ed7c59279c034b2bbe5c798
ZakharKolesnyk <zakhar.kolesnyk@gmail.com>
2017-03-19 16:54:12 +0200

    Refactoring FriendsController, LogoutController

--- social-media-web/src/main/java/com/gmail/kolesnyk/zakhar/controller/authorization/servlet/LogoutController.java
+++ social-media-web/src/main/java/com/gmail/kolesnyk/zakhar/controller/authorization/servlet/LogoutController.java
@@ -8,13 +8,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
-//@WebServlet(urlPatterns = {"/logout"})
-public class LogoutServlet/* extends HttpServlet */ {
-    //    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        resp.addCookie(new Cookie("auth", null));
-//        resp.sendRedirect("/static/login.jsp");
-//    }
+public class LogoutController {
     @RequestMapping(value = "/logout")
     public String logout(HttpServletResponse resp) {
         resp.addCookie(new Cookie("auth", null));
