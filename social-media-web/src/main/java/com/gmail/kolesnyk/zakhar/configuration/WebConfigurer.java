97b56742d8bb63ec64514fae8a0211b312b13a70
ZakharKolesnyk <zakhar.kolesnyk@gmail.com>
2017-05-24 11:31:04 +0300

    Changed HandlerInterceptor to HandlerInterceptorAdapter

--- social-media-web/src/main/java/com/gmail/kolesnyk/zakhar/configuration/WebConfigurer.java
+++ social-media-web/src/main/java/com/gmail/kolesnyk/zakhar/configuration/WebConfigurer.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 @EnableWebMvc
-public class StaticResourcesConfigurer extends WebMvcConfigurerAdapter {
+public class WebConfigurer extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
