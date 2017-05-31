0bafc3c6c53c8b2e132f5c79df629645ad82b68d
ZakharKolesnyk <zakhar.kolesnyk@gmail.com>
2017-05-31 09:45:56 +0300

    refactoring WebConfig.class

--- social-media-web/src/main/java/com/gmail/kolesnyk/zakhar/configuration/WebConfig.java
+++ social-media-web/src/main/java/com/gmail/kolesnyk/zakhar/configuration/WebConfig.java
@@ -1,17 +1,32 @@
 package com.gmail.kolesnyk.zakhar.configuration;
 
+
+import com.gmail.kolesnyk.zakhar.controller.handlers.RequestHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import javax.servlet.http.HttpSession;
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.gmail.kolesnyk.zakhar")
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private HttpSession httpSession;
+
     @Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
@@ -26,6 +41,46 @@ public class WebConfig {
         return new ReloadableResourceBundleMessageSource(){{
             setBasename("WEB-INF/locales/locale");
             setDefaultEncoding("UTF-8");
+        }};
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("/static/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestHandler()).addPathPatterns("/user/*");
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public RequestHandler requestHandler() {
+        return new RequestHandler();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        return new LocaleChangeInterceptor() {
+            @Override
+            protected Locale parseLocaleValue(String locale) {
+                Locale loc = super.parseLocaleValue(locale);
+                httpSession.setAttribute("locale", loc);
+                return loc;
+            }
+
+            {
+                setParamName("lang");
+            }
+        };
+    }
+
+    @Bean
+    public SessionLocaleResolver localeResolver() {
+        return new SessionLocaleResolver() {{
+            setDefaultLocale(Locale.ENGLISH);
         }};
     }
 }
